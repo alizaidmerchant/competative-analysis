1f51781353b8a4dedc2d1d987a001def01d66f7b
alizaidmerchant <151141339+alizaidmerchant@users.noreply.github.com>
2023-11-27 17:57:25 +0530

    Add files via upload

---
+++ tobasco_green_sauce.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = "https://shop.tabasco.com/products/green-sauce"
+r = requests.get(url)
+# print(r)
+
+soup = BeautifulSoup(r.text,"lxml")
+name = soup.find("h1",{"class":"value heading text-size--xlarge--fluid"})
+print(name.text)
+
+price = soup.find("span",{"class":"price"})
+print(price.text)
+
+des = soup.find("div",{"class":"product attribute description"})
+print(des.text)
+
+ingredients = soup.find("div",{"class":"product attribute ingredients"})
+print(ingredients.text)
+
+nutritions = soup.find("div",{"class":"product attribute nutrition_information col-md col-xs-12"})
+print(nutritions.text)
+
+# df = pd.DataFrame({"product_name":'name'})
+# print(df)
+print('aa')
